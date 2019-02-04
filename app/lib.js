d695e4b4a3c5bafca31ff4c829295afd26ad6e1d
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2019-02-04 02:02:22 +0000

    added new fn and fixed factorial fn

--- app/lib.js
+++ app/lib.js
@@ -12,12 +12,13 @@
 
         if(num){
 
-            for(let i  = 0; i <= num; i++){
-
-                i*=i
+            if(num === 0 || num === 1) return 1;
+            
+            for(let i = num - 1; i > 0; i--){
+                num*=i
             }
 
-            return i;
+            return num;
 
         } else {
 
@@ -30,23 +31,32 @@
         str = typeof(str) === 'string' && str.trim().length > 0 ? str : false;
 
         if(str){
-
+            
             let reverseArray = [];
-
             const strArray = str.split("");
 
             for(let i = strArray.length - 1; i >=0; i--){
-
                 reverseArray.push(strArray[i]);
             }
 
             return reverseArray.join("");
-
 
         } else {
 
             return false;
         }
     },
+    multipleTwoNumbers(num1, num2){
+        num1 = typeof(num1) === 'Number' ? num1 : false;
+        num2 = typeof(num2) === 'Number'? num2 : false;
+
+        if(num1 && num2){
+            
+            return num1 * num2;
+        } else {
+
+            return false;
+        }
+    }
 
  }
